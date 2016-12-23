4e6e04f3e8920890c537227541aff4616783a5db
Garrett Downs <choorp@gmail.com>
2016-12-23 13:54:26 -0500

    Out with the old...

--- src/client/app/shared/interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class AppConfig {
-	public id: string;
-	public title: string;
-	public url: string;
-	public iconUrl: string;
-}
