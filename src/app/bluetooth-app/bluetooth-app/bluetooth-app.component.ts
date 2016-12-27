519e0149c6f9409c080e6655f6140c18704b18a7
Garrett Downs <choorp@gmail.com>
2016-12-26 20:32:30 -0500

    Cleaned up Bluetooth

--- src/app/bluetooth-app/bluetooth-app/bluetooth-app.component.ts
+++ src/app/bluetooth-app/bluetooth-app/bluetooth-app.component.ts
@@ -40,22 +40,14 @@ export class BluetoothAppComponent implements OnInit {
     let step3 = setTimeout(() => {
       this.powerupStage = 3;
 
-      // Gray icon, plain text, spinner
       this.devices[0].connecting = true;
     }, 4000);
 
     let step4 = setTimeout(() => {
       this.powerupStage = 4;
 
-      // Blue icon, bold text
       this.devices[0].connecting = false;
       this.devices[0].connected = true;
     }, 6000);
-
-    let step5 = setTimeout(() => {
-      this.powerupStage = 5;
-
-      // this.networks[2].strength = 2;
-    }, 8000);
   }
 }
