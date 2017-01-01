c4a5cd6efb2436d5f79824084ba129d862b9b084
Garrett Downs <choorp@gmail.com>
2016-12-31 20:13:42 -0500

    Added Wifi to Device Menu

--- src/app/core/statusbar/statusbar.component.ts
+++ src/app/core/statusbar/statusbar.component.ts
@@ -1,9 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, trigger, transition, style, animate } from '@angular/core';
+import { WiFiService } from '../../shared/wifi.service';
+import { WiFiNetwork, WiFiState } from '../../shared/interfaces';
 
 @Component({
 	selector: 'status-bar',
 	templateUrl: './statusbar.component.html',
 	styleUrls: ['./statusbar.component.css'],
+	animations: [
+		trigger(
+			'fadeIn', [
+				transition(':enter', [
+					style({opacity: 0}),
+					animate('300ms', style({opacity: 1}))
+				]),
+				transition(':leave', [
+					style({opacity: 1}),
+					animate('300ms', style({opacity: 0}))
+				])
+			]
+		)
+	]
 })
 export class StatusbarComponent implements OnInit {
 	isReady: boolean = false;
@@ -13,7 +29,34 @@ export class StatusbarComponent implements OnInit {
 	batteryIconUrl: string = 'assets/ui/battery-0.png';
 	updateTimer: any;
 
-	constructor() { }
+	wifiNetworks: WiFiNetwork[] = [];
+	wifiState: WiFiState;
+	connectedNetworkSSID: string = '';
+	showWifiSubMenu: boolean = false;
+	bluetoothStatus: Object = {};
+	showBluetoothSubMenu: boolean = false;
+
+	constructor(private _systemService: WiFiService) {
+		_systemService.state$.subscribe((state: WiFiState) => {
+			// console.log('statusbar', 'new wifi state', state);
+			this.wifiState = state;
+
+			if (state.state >= 2) {
+				let connectedNetwork: WiFiNetwork = this.wifiNetworks.find(a => a.connecting == true || a.connected == true);
+				this.connectedNetworkSSID = connectedNetwork.ssid;
+			}
+		});
+
+		// this.wifiNetworks = _systemService.wifiNetworks;
+		_systemService.networks$.subscribe((networks: WiFiNetwork[]) => {
+			// console.log('statusbar', 'wifi networks updated');
+			this.wifiNetworks = networks;
+		});
+
+		// _systemService.bluetoothStatus$.subscribe(status => {
+		// 	this.bluetoothStatus = status;
+		// });
+	}
 
 	ngOnInit() {
 		this.dmSettings.date = new Date();
@@ -25,6 +68,9 @@ export class StatusbarComponent implements OnInit {
 		}, 5000)
 
 		this.isReady = true;
+
+		this._systemService.setState(0);
+		// this._systemService.setBluetoothStatus(0);
 	}
 
 	checkBatteryStatus() {
@@ -49,6 +95,29 @@ export class StatusbarComponent implements OnInit {
 		if (this.isDeviceMenuOpen) {
 
 		}
+	}
+
+	toggleSubMenu(menu) {
+		switch (menu) {
+			case 'wifi':
+				this.showWifiSubMenu = !this.showWifiSubMenu;
+				break;
+			case 'bluetooth':
+				this.showBluetoothSubMenu = !this.showBluetoothSubMenu;
+				break;
+		}
+	}
+
+	toggleWifiState() {
+		if (this.wifiState.state == 0) {
+			this._systemService.turnOnWifi();
+		} else {
+			this._systemService.turnOff();
+		}
+	}
+
+	connectToWifiNetwork(network: WiFiNetwork) {
+		this._systemService.connectToNetwork(network.ssid);
 	}
 
 	goFullScreen() {
